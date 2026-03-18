c42fc61196c9d64f02977cde9af7666013c4aab9
sebastianbrookess <brookes.se@northeastern.edu>
2026-03-18 11:30:28 -0400

    Replace pin emoji with SVG icon and align with flex wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,10 @@ export default function App() {
             {location.status === "resolved" && !showManualInput && (
               <>
                 <p className="location-label">
-                  📍 {location.data.locationName}
+                  <span className="location-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#B46A46" aria-hidden="true"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
+                    {location.data.locationName}
+                  </span>
                 </p>
                 <button
                   type="button"
