52c8195cb3c1dace672f9cffc3944f7bc8ec35e3
sebastianbrookess <brookes.se@northeastern.edu>
2026-03-20 19:04:43 -0400

    config for vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import App from "./App";
 
-const convex = new ConvexReactClient(import.meta.env.CONVEX_URL as string);
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
